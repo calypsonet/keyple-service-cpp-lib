8f74db553de288352d83595ac8926bfd63287b3b
Alexandre MUNSCH <alexandre.munsch@gmail.com>
2021-10-30 15:53:28 +0200

    Feat: added missing class

---
+++ src/main/SmartCardServiceProvider.h
@@ -0,0 +1,46 @@
+/**************************************************************************************************
+ * Copyright (c) 2021 Calypso Networks Association https://calypsonet.org/                        *
+ *                                                                                                *
+ * See the NOTICE file(s) distributed with this work for additional information regarding         *
+ * copyright ownership.                                                                           *
+ *                                                                                                *
+ * This program and the accompanying materials are made available under the terms of the Eclipse  *
+ * Public License 2.0 which is available at http://www.eclipse.org/legal/epl-2.0                  *
+ *                                                                                                *
+ * SPDX-License-Identifier: EPL-2.0                                                               *
+ **************************************************************************************************/
+
+#pragma once
+
+#include "SmartCardService.h"
+
+namespace keyple {
+namespace core {
+namespace service {
+
+/**
+ * Provider of the SmartCardService.
+ *
+ * @since 2.0.0
+ */
+class SmartCardServiceProvider final {
+public:
+    /**
+     * Gets the unique instance of SmartCardService.
+     *
+     * @return A not null reference.
+     * @since 2.0.0
+     */
+    static SmartCardService& getService();
+
+private:
+    /**
+     * Private constructor
+     */
+    SmartCardServiceProvider();
+
+};
+
+}
+}
+}
